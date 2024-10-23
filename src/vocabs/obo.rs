e6619fd05952af0faf3b4f415f58b132069011db
curoli <oliverr@broadinstitute.org>
2024-10-23 12:09:20 -0400

    Adding UBERON support.

--- src/vocabs/obo.rs
+++ src/vocabs/obo.rs
@@ -3,7 +3,28 @@ use crate::model::iri::Iri;
 pub const PREFIX: &str = "obo";
 pub const NAMESPACE: &Iri = &Iri::new_str("http://purl.obolibrary.org/obo/");
 
-pub fn create_iri(id: u64) -> Iri {
-    NAMESPACE.join(format!("RO_{:07}", id))
+pub mod ns {
+    use crate::model::iri::Iri;
+    use crate::vocabs::obo::NAMESPACE;
+
+    pub const RO: &Iri = &NAMESPACE.join_str("RO_");
+    pub const UBERON: &Iri = &NAMESPACE.join_str("UBERON_");
+
+}
+
+pub enum Ontology {
+    RO, UBERON
+}
+
+impl Ontology {
+    pub fn namespace(&self) -> &'static Iri {
+        match self {
+            Ontology::RO => ns::RO,
+            Ontology::UBERON => ns::UBERON
+        }
+    }
+    pub fn create_iri(&self, id: u64) -> Iri {
+        self.namespace().join(format!("{:07}", id))
+    }
 }
 
