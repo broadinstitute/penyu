7abccc16c71d78889e24453b0a62c7b7cf93d2b8
curoli <oliverr@broadinstitute.org>
2024-11-07 16:55:37 -0500

    Add encoding characters for local names.

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,4 @@ pub mod model;
 pub mod writer;
 pub mod error;
 pub mod vocabs;
-mod syntax;
+pub mod syntax;
--- src/syntax.rs
+++ src/syntax.rs
@@ -55,3 +55,43 @@ fn is_local_name_first_char(c: char) -> bool {
 fn is_local_name_later_char(c: char) -> bool {
     c.is_alphanumeric() || c == '_' || c == '-' || c == '.'
 }
+pub fn encode_local_name<I: Iterator<Item=char>>(chars: &mut I) -> String {
+    let mut encoded = String::new();
+    if let Some(first) = chars.next() {
+        if is_local_name_first_char(first) {
+            encoded.push(first);
+        } else {
+            encoded.push_str(&encode_char(first));
+        }
+        for c in chars {
+            if is_local_name_later_char(c) {
+                encoded.push(c);
+            } else {
+                encoded.push_str(&encode_char(c));
+            }
+        }
+    }
+    encoded
+}
+
+pub fn encode_char(c: char) -> String {
+    match c {
+        '\t' => "\\t".to_string(),
+        '\n' => "\\n".to_string(),
+        '\r' => "\\r".to_string(),
+        '\x0C' => "\\f".to_string(),
+        '\'' => "\\'".to_string(),
+        '"' => "\\\"".to_string(),
+        '\\' => "\\\\".to_string(),
+        _ => {
+            let c = c as u32;
+            if c < 256 {
+                format!("%{:02X}", c)
+            } else if c < 256 * 256 {
+                format!("\\u{:04X}", c)
+            } else {
+                format!("\\U{:08X}", c)
+            }
+        }
+    }
+}
